764c2ab791f56c6d8f3075058aeda838e3f0f66c
RYan <ryan.scarborough1996@gmail.com>
2022-04-27 16:46:37 -0400

    Finish writing DOM manipulation functionality for ajax requests and add a request for getting all employees.

--- index.php
+++ index.php
@@ -19,11 +19,26 @@ $router->add("/", "POST", function () {
 });
 
 $router->add("/employees", "GET", function () {
+	// Check Credentials
 	require_once "views/employees.html";
 });
 
+$router->add("/employees/data", "GET", function () {
+	// Check Credentials
+	// $data = $db->getAllEmployees();
+	$data[] = array("id" => 1, "name" => "1", "title" => "temp", "hired" => "2022-04-27");
+	$data[] = array("id" => 2, "name" => "2", "title" => "temp", "hired" => "2022-04-27");
+	$data[] = array("id" => 3, "name" => "3", "title" => "temp", "hired" => "2022-04-27");
+
+	echo json_encode($data);
+});
+
 $router->add("/employees", "POST", function () {
-	echo "true";
+	$data = array(
+		"success" => "true",
+		"id" => "1"
+	);
+	echo json_encode($data);
 	// Check Credentials
 	// if add to db works
 	//	echo "true";
@@ -32,7 +47,10 @@ $router->add("/employees", "POST", function () {
 });
 
 $router->add("/employees", "PUT", function () {
-	echo "true";
+	$data = array(
+		"success" => "true"
+	);
+	echo json_encode($data);
 	// Check Credentials
 	// if update to db works
 	//	echo "true";
--- javascript/employees.js
+++ javascript/employees.js
@@ -1,19 +1,24 @@
+document.addEventListener('DOMContentLoaded', function() {
+	    makeRequest("/employees/data", "GET", handleGetEmployees);
+});
+
 function getEmployeeData(el) {
 	// Find the card containing the employee and get the data from hidden input fields
 	return Array.from(el.closest(".card").querySelectorAll("input"));
 }
 
-function findCardInputValue(employeeData, key) {
-	return employeeData.filter(function(x) { return x.name == key; })[0].value;
+function findCardInput(employeeData, key) {
+	return employeeData.filter(function(x) { return x.name == key; })[0];
 }
 
 function getEditData(el) {
 	var employeeData = getEmployeeData(el);
+	el.closest(".card").id = "toBeEdited";
 
-	var name = findCardInputValue(employeeData, "name");
-	var title = findCardInputValue(employeeData, "title");
-	var hired = findCardInputValue(employeeData, "hired");
-	var id = findCardInputValue(employeeData, "employeeID");
+	var name = findCardInput(employeeData, "name").value;
+	var title = findCardInput(employeeData, "title").value;
+	var hired = findCardInput(employeeData, "hired").value;
+	var id = findCardInput(employeeData, "id").value;
 
 	document.getElementById("editName").value = name;
 	document.getElementById("editTitle").value = title;
@@ -28,7 +33,7 @@ function submitDelete(el){
 
 	el.closest(".card").id = "toBeDeleted";
 	var employeeData = getEmployeeData(el);
-	var id = findCardInputValue(employeeData, "employeeID");
+	var id = findCardInput(employeeData, "id").value;
 	var data = {};
 	data["id"] = id;
 
@@ -40,24 +45,23 @@ function checkDeleteOk(response) {
 		removeAlert();
 		myAlert = createAlert("Employee Deleted","success");
 		document.getElementById("alertBox").appendChild(myAlert);
-		document.getElementById("toBeDeleted").remove();
-		// Move cards over
+		document.getElementById("toBeDeleted").parentElement.remove();
 	} else {
-		alert(response);
+		alert("Something went wrong");
 	}
 }
 
-function submitAddOrEdit(e, method){
-	e.preventDefault();
-
+function getModalData(method) {
+	var data = {};
 	var prefix = "";
 	if (method == "POST") {
 		prefix = "add";
 	} else {
 		prefix = "edit";
+		var idInput = document.getElementById("editID");
+		data[idInput.name] = idInput.value;
 	}
 
-	var data = {};
 	var nameInput = document.getElementById(prefix + "Name");
 	var titleInput = document.getElementById(prefix + "Title");
 	var hiredInput = document.getElementById(prefix + "Hired");
@@ -65,25 +69,85 @@ function submitAddOrEdit(e, method){
 	data[titleInput.name] = titleInput.value;
 	data[hiredInput.name] = hiredInput.value;
 
-	makeRequest("/employees", method.toUpperCase(), checkAddOrEditOk, data);
+	return data;
 }
 
-function checkAddOrEditOk(response, method) {
-	if (response == "true") {
+function submitAddOrEdit(e, method){
+	e.preventDefault();
+	var data = getModalData(method);
+	makeRequest("/employees", method.toUpperCase(), handleAddOrEdit, data);
+}
+
+function handleAddOrEdit(json, method) {
+	var responseData = JSON.parse(json);
+	if (responseData["success"] == "true") {
+		var data = getModalData(method);
 		var message = "";
 		if (method == "POST") {
 			message = "Added";
-			// Add card
+			data["id"] = responseData["id"];
+			var newCardColumn = createCardColumn(data);
+			insertCardColumn(newCardColumn);
 		} else {
 			message = "Updated";
-			// Update card
+			var card = document.getElementById("toBeEdited");
+			card.querySelector(".card-header").innerHTML = data["name"];
+			card.querySelector(".title").innerHTML = "Title: <strong>" + data["title"] + "</strong>";
+			card.querySelector(".date-hired").innerHTML = "Hired Date: <strong>" + data["hired"] + "</strong>";
+
+			var employeeData = getEmployeeData(card);
+			findCardInput(employeeData, "name").value = data["name"];
+			findCardInput(employeeData, "title").value = data["title"];
+			findCardInput(employeeData, "hired").value = data["hired"];
+
+			card.removeAttribute("id");
 		}
 
 		removeAlert();
 		myAlert = createAlert("Employee " + message,"success");
 		document.getElementById("alertBox").appendChild(myAlert);
 	} else {
-		alert(response);
+		alert("Something went wrong");
 	}
 	Array.from(document.getElementsByClassName("close-modal")).forEach(element => element.click());
+}
+
+function createCardColumn(data) {
+	var column = document.createElement("div");
+	column.classList.add("col-xs-12", "col-md-4");
+
+	column.innerHTML = '<div class="card my-3 mx-3 shadow text-center">' +
+        '	<h5 class="card-header bg-primary text-white text-center">' + data["name"] + '</h5>' +
+        '	<div class="d-flex justify-content-end">' +
+        '	        <button class="btn btn-outline-danger btn-close m-1" title="Delete Employee" onclick="submitDelete(this)"></button>' +
+        '	</div>' +
+        '	<p class="title">Title: <strong>' + data["title"] + '</strong></p>' +
+        '	<p class="date-hired">Date Hired: <strong>' + new Date(data["hired"]).toLocaleDateString("en-US") + '</strong></p>' +
+        '	<input type="hidden" name="id" value="' + data["id"] + '">' +
+        '	<input type="hidden" name="name" value="' + data["name"] + '">' +
+        '	<input type="hidden" name="title" value="' + data["title"] + '">' +
+        '	<input type="hidden" name="hired" value="' + data["hired"] + '">' +
+        '	<div class="card-footer">' +
+        '	        <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#editModal" onclick="getEditData(this);">Edit Employee</button>' +
+        '	</div>' +
+	'</div>';
+	return column;
+}
+
+function insertCardColumn(column) {
+	document.getElementById("mainRow").firstElementChild.after(column);
+}
+
+function handleGetEmployees(json) {
+	if (json != null){
+		var responseData = JSON.parse(json);
+
+		// Sort by name in REVERSE alphabetical order.
+		// This is because the first column added will end up at the bottom of the page.
+		responseData = responseData.sort((a, b) => a["name"] > b["name"] ? -1 : 1);
+		console.log(responseData);
+		responseData.forEach(employee => insertCardColumn(createCardColumn(employee)));
+	} else {
+		alert("Something went wrong");
+	}
 }
