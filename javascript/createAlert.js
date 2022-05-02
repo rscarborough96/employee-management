ca4faf231a0ca8aa5c1c00a95b592723328b983e
RYan <ryan.scarborough1996@gmail.com>
2022-05-01 22:15:42 -0400

    Add timer to automatically remove alerts after 5 seconds.

--- javascript/createAlert.js
+++ javascript/createAlert.js
@@ -2,6 +2,12 @@ function createAlert(message, type){
 	var myAlert = document.createElement("div");
 	var removeButton = '  <button id="closeAlert" type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>';
 	myAlert.innerHTML = '<div class="alert alert-' + type + ' alert-dismissible fade show" role="alert">' + message + removeButton +'</div>';
+
+	// Automatically remove alert after some time
+	setTimeout(() => {
+		myAlert.remove();
+	}, "5000");
+
 	return myAlert;
 }
 
